5050c8925ffd3fccf19340921269efb00efd6c70
viniciusym <viniciusyamate5@gmail.com>
2025-03-13 18:11:24 +0000

    refactor: add interfaces for services and professionals

---
+++ src/interfaces/Professional.ts
@@ -0,0 +1,9 @@
+export default interface Professional {
+  id_servico: number;
+  id_profissionais: number;
+  nome: string;
+  numero: number;
+  local: string;
+  cidade: string;
+  imagem_url: string;
+}
---
+++ src/interfaces/Servico.ts
@@ -0,0 +1,4 @@
+export default interface Servico {
+  id: number;
+  nome: string;
+}
