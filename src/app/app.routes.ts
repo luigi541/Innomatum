6a2e3ba8a2f32e25b06bc444df427e66d467e57f
viniciusym <viniciusyamate5@gmail.com>
2025-03-14 15:24:17 +0000

    feat: add 'profissionais' route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AgendamentoServicoComponent } from './agendamento-servico/agendamento-servico.component';
+import { ProfessionalsPageComponent } from './professionals-page/professionals-page.component';
 
 export const routes: Routes = [
-    { path: 'agendamento/:id', component: AgendamentoServicoComponent }, 
+    { path: 'agendamento/:id', component: AgendamentoServicoComponent },
+    { path: 'profissionais', component: ProfessionalsPageComponent},
 ];
