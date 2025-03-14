292c1da489f79be9ecb1ab79db2722f9140ec5f4
Renato Azevedo <a28549@alunos.ipca.pt>
2025-03-14 10:00:08 +0000

    Correcao de erros

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes'; // Importa as rotas
+import { provideHttpClient } from '@angular/common/http'; // Necessário para o ApiService
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes), 
+    provideHttpClient() 
+  ]
+}).catch((err) => console.error(err));
