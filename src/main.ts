105ab4281b4d137767fe3a40d6546b4126b6cbbc
Seiixas1 <seixaspedro2@gmail.com>
2025-03-14 15:32:48 +0000

    homepage

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { HomeComponent } from './app/home/home.component';
 
-bootstrapApplication(AppComponent, {
+bootstrapApplication(HomeComponent, {
   ...appConfig,
   providers: [provideHttpClient()],
 }).catch((err) => console.error(err));
